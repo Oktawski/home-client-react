36b0f890296cdc9e75b7ac97fa5e3224426744b8
Oktawski <oktawian.perlowski@gmail.com>
2022-12-11 00:31:14 +0100

    Implemented AddProduct component

--- src/components/groceries/AddProduct.tsx
+++ src/components/groceries/AddProduct.tsx
@@ -1,9 +1,16 @@
 import { LoadingButton } from "@mui/lab";
+import { FormControl, Grid, MenuItem, TextField } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
 import { Category } from "../../models/Groceries";
 import { ProductRequest } from "../../requests/groceries.requests";
 import { groceryService } from "../../services/groceries/grocery.service";
 import { categoryService } from "../../services/groceries/product.service";
+
+const categoriesMock = [
+    "Vegetable",
+    "Fruit",
+    "Candy"
+];
 
 interface AddProductProps {
     categories: Array<Category>
@@ -34,12 +41,30 @@ export function AddProduct(props: AddProductProps) {
         setLoading(false);
     }
 
-    if (loading)
-        return (<div>Loading...</div>)
-
     return (
-        <>
-            Add new Product 
-        </>
+        <Grid container alignItems="center" justifyContent="center" spacing={1} component="form" onSubmit={handleSubmit}>
+            <Grid item xs={10} sm={3}>
+                <TextField fullWidth label="Name" variant="outlined" size="small"/>
+            </Grid>
+            <Grid item xs={10} sm={3}>
+                <TextField fullWidth select label="Category" variant="outlined" size="small" >
+                    {categoriesMock.map(option => (
+                        <MenuItem key={option} value={option}>
+                            {option}
+                        </MenuItem>
+                    ))}
+                </TextField>
+            </Grid>
+            <Grid item xs={10} sm={3}>
+                <TextField fullWidth label="Quantity" type="number" variant="outlined" size="small" />
+            </Grid>
+
+            <Grid item xs={10} sm={'auto'} alignContent="center">
+                <LoadingButton fullWidth type="submit" loading={loading}>
+                    Add
+                </LoadingButton>
+            </Grid>
+
+        </Grid>
     );
 }
