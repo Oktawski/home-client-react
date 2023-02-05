979a99f2f97ec38811dbb545a9ee3a2652cbff75
Oktawski <oktawian.perlowski@gmail.com>
2023-02-05 22:02:27 +0100

    Implemented icons for categories

--- src/components/groceries/Icons.tsx
+++ src/components/groceries/Icons.tsx
@@ -1,7 +1,24 @@
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { faDrumstickBite, faLeaf } from "@fortawesome/free-solid-svg-icons";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { 
+    faDrumstickBite, 
+    faLeaf, 
+    faAppleWhole,
+    faMortarPestle,
+    faCookieBite,
+    faGlassWater
+} from "@fortawesome/free-solid-svg-icons";
 
-export const icons: { [type: string]: IconProp} = {
-    "Vegetables": faLeaf,
-    "Meat": faDrumstickBite
+type IconAndColorProp = {
+    icon: IconDefinition,
+    color: string 
 }
+
+export const icons: { [type: string]: IconAndColorProp } = {
+    "Vegetables": { icon: faLeaf, color: "green" },
+    "Fruit": { icon: faAppleWhole, color: "#e60d5b" },
+    "Meat": { icon: faDrumstickBite, color: "#5f1b28" },
+    "Vegan": { icon: faLeaf, color: "green" },
+    "Seasoning": { icon: faMortarPestle, color: "brown" },
+    "Sweet": { icon: faCookieBite, color: "#f46197" },
+    "Drink": { icon: faGlassWater, color: "#219ebc" }
+};
--- src/components/groceries/ProductList.tsx
+++ src/components/groceries/ProductList.tsx
@@ -21,8 +21,12 @@ export function ProductList(props: ProductListProps) {
     const columns: GridColDef[] = [
         { field: 'id', headerName: 'ID', flex: 1 },
         { field: 'name', headerName: 'Name', flex: 2 },
-        { field: 'category', headerName: 'Category', flex: 1, renderCell: (params) =>
-            <FontAwesomeIcon icon={icons[params.row['category']]} /> 
+        { field: 'category', headerName: 'Category', flex: 1, align: 'center', renderCell: (params) =>
+            <FontAwesomeIcon 
+                icon={ icons[params.row['category']].icon } 
+                color={ icons[params.row['category']].color } 
+                size="xl"
+            /> 
         },
         {
             field: 'quantity',
@@ -60,13 +64,15 @@ export function ProductList(props: ProductListProps) {
 
 
     return (
-        <div style={{ height: 400 }}>
+        <div style={{ height: 500, width: '100%' }}>
             <div style={{ display: 'flex', height: '100%' }}>
-                <DataGrid 
-                    columns={columns} 
-                    rows={props.products}
-                    experimentalFeatures={{ newEditingApi: true }}
-                />
+                <div style={{ flexGrow: 1 }}>
+                    <DataGrid 
+                        columns={columns} 
+                        rows={props.products}
+                        experimentalFeatures={{ newEditingApi: true }}
+                    />
+                </div>
             </div>
         </div>
     );
